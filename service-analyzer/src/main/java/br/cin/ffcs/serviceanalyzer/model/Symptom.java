977dbaa436f5a45ffc0ee2fea5b1b3106664b1d7
Fagner Fernandes <ffcs@cin.ufpe.br>
2022-02-21 09:42:22 -0300

    Refactoring do service monitor.

--- service-analyzer/src/main/java/br/cin/ffcs/serviceanalyzer/model/Symptom.java
+++ service-analyzer/src/main/java/br/cin/ffcs/serviceanalyzer/model/Symptom.java
@@ -1,5 +1,5 @@
 package br.cin.ffcs.serviceanalyzer.model;
 
 public enum Symptom {
-    WARNING, ERROR, OK
+    INFO, ALERT, ERROR
 }
--- service-monitor/src/main/java/br/cin/ffcs/servicemonitor/service/MonitorTaskService.java
+++ service-monitor/src/main/java/br/cin/ffcs/servicemonitor/service/MonitorTaskService.java
@@ -11,17 +11,20 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class MonitorTaskService {
     private static final Logger logger = LoggerFactory.getLogger(MonitorTaskService.class);
 
-    private static final String DOCKER_STATS_CMD = "docker service ls";
+    private static final String DOCKER_STATS_CMD = "docker stats --no-stream --format \"table {{.NAME}}\\t{{.CPUPerc}}\"";
 
     @Autowired
     private MetricRepository metricRepository;
 
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(fixedRate = 10000)//runs every 10 segs
     public void eventCollector(){
         Process process;
         try {
@@ -30,24 +33,43 @@ public class MonitorTaskService {
             String linha;
             StringBuilder sb = new StringBuilder();
             while( (linha = reader.readLine())!=null){
-              sb.append(linha);
+              sb.append(linha+"|");
             }
-            symptomGenerator(sb);
+            String[] tempArray = sb.toString().split("|");
+            List<String> lista = Arrays.asList(tempArray);
+            List<Metric> processedData = eventPreProcessing(lista);
+            symptomGenerator(processedData);
         } catch (IOException ioe) {
             logger.info(ioe.getMessage());
         }
-
     }
 
     //receive, parse and save data
-    private void symptomGenerator(StringBuilder sb) {
-        //create metric obj and persists
-        Metric m = Metric.builder().build();
-        updateMSM(m);
-
-        //send symptoms to analyzer
+    private void symptomGenerator(List<Metric> processedData) {
         MonitorSymptomSenderService symptomSenderService = new MonitorSymptomSenderService();
-        symptomSenderService.sendSymptoms(m);
+        for (Metric m : processedData) {
+            //update knowledge base
+            updateMSM(m);
+
+            //send symptoms to analyzer
+            symptomSenderService.sendSymptoms(m);
+        }
+
+    }
+    /*
+    process data, normalize and aggregate
+     */
+    private List<Metric> eventPreProcessing(List<String> lista) {
+        List<Metric> data = new ArrayList<>();
+        for (String l : lista){
+            //create metric obj and persists
+            Metric m = Metric.builder()
+                             .chave("CPU_UTILIZATION")
+                             .valor(l)
+                             .build();
+            data.add(m);
+        }
+        return data;
     }
 
     public void updateMSM(Metric m){
